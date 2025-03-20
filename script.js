1789be79c5115616e4ba11774bf83762e0a67ffa
MoonLightCodes <poornachander295@gmail.com>
2025-03-20 12:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+window.addEventListener('load',()=>{
+	let b = document.createElement('button');
+	b.setAttribute('id','btn');
+	b.setAttribute('type','submit');
+	b.classList.add('btn');
+	b.innerText='Submit';
+	document.querySelector('div').append(b);
+	
+})
